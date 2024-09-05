3ad8ff13cf4bdc3eb707a584502f9da5488fcfae
asiajones8 <asia.jones@sciacademy.org>
2024-09-05 15:04:25 +0000

    TODO 1 completed

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,7 +38,7 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
+createPlatform(500 ,300 , 200, 20); //short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
 
 
     
